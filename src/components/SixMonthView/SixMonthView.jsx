c800e8035a5d9ad00052d62c702dba77faeff60b
Courtney Smith <Court@a1226.home>
2024-05-13 14:40:15 +0100

    Setup quartly view

--- src/components/SixMonthView/SixMonthView.jsx
+++ src/components/SixMonthView/SixMonthView.jsx
@@ -4,22 +4,33 @@ import "gantt-task-react/dist/index.css";
 export default function SixMonthView() {
   let tasks = [
     {
-      start: new Date(2024, 1, 1),
-      end: new Date(2024, 1, 2),
-      name: "Idea",
+      start: new Date(2024, 1, 7),
+      end: new Date(2024, 2, 8),
+      name: "Cycle to Work Scheme",
       id: "Task 0",
       type: "task",
-      progress: 45,
       isDisabled: true,
-      styles: { progressColor: "#ffbb54", progressSelectedColor: "#ff9e0d" },
+      progress: 100,
+      styles: { progressColor: "#CF9FFF", progressSelectedColor: "#CF9FFF" },
+    },
+
+    {
+      start: new Date(2024, 3, 7),
+      end: new Date(2024, 6, 8),
+      name: "New Employee Induction Programme",
+      id: "Task 1",
+      type: "task",
+      isDisabled: true,
+      progress: 100,
+      styles: { progressColor: "#FF5C00", progressSelectedColor: "#FF5C00" },
     },
   ];
   return (
     <Gantt
       tasks={tasks}
       viewMode={ViewMode.Month}
-      viewDate={new Date(2024, 3, 1)}
-      columnWidth={400}
+      viewDate={new Date()}
+      columnWidth={500}
       rowHeight={50}
       TooltipContent={() => null}
       TaskListHeader={() => null}
