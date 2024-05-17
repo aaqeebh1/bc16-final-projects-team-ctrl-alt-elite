f9924d3f67e32bb7ea419322bb3e346524ec22c1
aaqeebh1 <24283167+aaqeebh1@users.noreply.github.com>
2024-05-17 11:49:44 +0100

    filtered yearly view css updates

--- src/components/YearView/YearView.jsx
+++ src/components/YearView/YearView.jsx
@@ -1,78 +1,48 @@
-import {Gantt, ViewMode} from "gantt-task-react";
+import { Gantt, ViewMode } from "gantt-task-react";
 import "gantt-task-react/dist/index.css";
-import './YearView.css'
+import "./YearView.css";
 import tasks from "../../assets/DummyData.js";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
+export default function YearView({
+  selectedDepartments,
+  setViewDate,
+  viewDate,
+}) {
+  const [filterTasks, setFilterTasks] = useState([]);
+  // const [viewDate, setViewDate] = useState(new Date());
+  const yearlyTasks = [];
+  tasks.map((task) => {
+    if (task.type === "project" || task.type === "milestone") {
+      yearlyTasks.push(task);
+    }
+  });
 
-export default function YearView({selectedDepartments}) {
-    const [filterTasks, setFilterTasks] = useState([]);
-    const [viewDate, setViewDate] = useState(new Date());
-
-
-    useEffect(() => {
-        const filteredTasks = tasks.filter((task) =>
-            Object.keys(selectedDepartments).some(
-                (dept) => dept === task.department && selectedDepartments[dept]
-            )
-        );
-        setFilterTasks(filteredTasks);
-    }, [selectedDepartments]);
-    const deleteSidebar = {
-        TooltipContent: () => null,
-        TaskListHeader: () => null,
-        TaskListTable: () => null,
-    };
-
-
-    return (
-        <>
-            <div className="buttonWrapper">
-                <button className="monthButton" type="button" onClick={() => {
-                    let newDate = new Date(viewDate);
-
-
-                    if (newDate.getMonth() === 0) {
-                        newDate.setFullYear(newDate.getFullYear() - 1);
-                        newDate.setMonth(11);
-                    } else {
-                        newDate.setMonth(newDate.getMonth() - 1);
-                    }
-
-
-                    setViewDate(newDate);
-                }}>{"<"}
-                </button>
-                <button className="monthButton" type="button" onClick={() => {
-                    let newDate = new Date(); // Get current date
-                    setViewDate(newDate);
-                }}>Today
-                </button>
-                <button className="monthButton" type="button" onClick={() => {
-                    let newDate = new Date(viewDate);
-
-
-                    if (newDate.getMonth() === 11) {
-                        newDate.setFullYear(newDate.getFullYear() + 1);
-                        newDate.setMonth(0);
-                    } else {
-                        newDate.setMonth(newDate.getMonth() + 1);
-                    }
-
-
-                    setViewDate(newDate);
-                }}>{">"}
-                </button>
-            </div>
-            <Gantt
-                tasks={filterTasks.length ? filterTasks : tasks}
-                viewMode={ViewMode.Month}
-                columnWidth={window.innerWidth / 12.75}
-                rowHeight={50}
-                viewDate={viewDate}
-                ganttHeight={window.innerHeight * 0.66}
-                {...deleteSidebar}
-            />
-        </>
+  useEffect(() => {
+    const filteredTasks = yearlyTasks.filter((task) =>
+      Object.keys(selectedDepartments).some(
+        (dept) => dept === task.department && selectedDepartments[dept]
+      )
     );
+    setFilterTasks(filteredTasks);
+  }, [selectedDepartments]);
+  const deleteSidebar = {
+    TooltipContent: () => null,
+    TaskListHeader: () => null,
+    TaskListTable: () => null,
+  };
+
+  return (
+    <>
+      <Gantt
+        tasks={filterTasks.length ? filterTasks : yearlyTasks}
+        viewMode={ViewMode.Month}
+        columnWidth={window.innerWidth / 12.75}
+        rowHeight={50}
+        viewDate={viewDate}
+        ganttHeight={window.innerHeight * 0.66}
+        {...deleteSidebar}
+      />
+    </>
+  );
 }
